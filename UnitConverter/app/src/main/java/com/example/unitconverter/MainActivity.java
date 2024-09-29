f1f74dd289555af86d241167a838294f724dff34
guehara-code <gil_uehara@yahoo.com.br>
2024-09-29 20:58:40 -0300

    java converter app - convert function implementation

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.unitconverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    Button btn;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textview);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editText.getText().toString();
+                double kilos = Double.parseDouble(inputText);
+                double pounds = makeConversion(kilos);
+                textView.setText("" + pounds);
+            }
         });
+
+    }
+
+    public double makeConversion(double kilos) {
+        return kilos * 2.20462;
     }
 }
