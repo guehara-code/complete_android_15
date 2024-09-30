641b04c5271a94c771ce23eb62946358e805e0f3
guehara-code <gil_uehara@yahoo.com.br>
2024-09-30 17:28:05 -0300

    java french teacher app - ui created

--- LuckyNumber/app/src/main/java/com/example/luckynumber/SecondActivity.java
+++ LuckyNumber/app/src/main/java/com/example/luckynumber/SecondActivity.java
@@ -1,6 +1,10 @@
 package com.example.luckynumber;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,59 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class SecondActivity extends AppCompatActivity {
+
+    TextView welcomeTxt, luckyNumberTxt;
+    Button share_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        welcomeTxt = findViewById(R.id.textView2);
+        luckyNumberTxt = findViewById(R.id.lucky_number_txt);
+        share_btn = findViewById(R.id.share_btn);
+
+        Intent i = getIntent();
+        String userName = i.getStringExtra("name");
+
+        int random_num = generateRandomNumber();
+        luckyNumberTxt.setText("" + random_num);
+
+        share_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareData(userName, random_num);
+            }
         });
+
+    }
+
+    public int generateRandomNumber() {
+        Random random = new Random();
+        int upper_limit = 1000;
+
+        int randomNumberGenerated = random.nextInt(upper_limit);
+        return randomNumberGenerated;
+    }
+
+    public void shareData(String username, int randomNum) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+
+        i.putExtra(Intent.EXTRA_SUBJECT, username + " got lucky today!");
+        i.putExtra(Intent.EXTRA_TEXT, "His lucky number is: " + randomNum);
+
+        startActivity(Intent.createChooser(i, "Choose a Platform"));
+
     }
 }
