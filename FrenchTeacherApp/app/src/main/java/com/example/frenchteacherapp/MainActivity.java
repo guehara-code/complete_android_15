1ab32c82cb33bd18742ef677be65a21ae0d57740
guehara-code <gil_uehara@yahoo.com.br>
2024-10-01 08:26:31 -0300

    java french teacher app - all buttons functions implemented

--- FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.java
+++ FrenchTeacherApp/app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -11,20 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button blackBtn, yellowBtn, redBtn, purpleBtn, greenBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
         blackBtn = findViewById(R.id.blackBtn);
         redBtn = findViewById(R.id.redBtn);
@@ -32,16 +32,48 @@ public class MainActivity extends AppCompatActivity {
         purpleBtn = findViewById(R.id.purpleBtn);
         greenBtn = findViewById(R.id.greenBtn);
 
-        redBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = MediaPlayer.create(
-                        getApplicationContext(),
-                        R.raw.red
-                );
-                mediaPlayer.start();
-            }
-        });
+//        redBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MediaPlayer mediaPlayer = MediaPlayer.create(
+//                        getApplicationContext(),
+//                        R.raw.red
+//                );
+//                mediaPlayer.start();
+//            }
+//        });
+
+        redBtn.setOnClickListener(this);
+        blackBtn.setOnClickListener(this);
+        yellowBtn.setOnClickListener(this);
+        purpleBtn.setOnClickListener(this);
+        greenBtn.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        int clickedBtnId = v.getId();
+
+        if (clickedBtnId == R.id.redBtn){
+            PlaySounds(R.raw.red);
+        } else if (clickedBtnId == R.id.blackBtn) {
+            PlaySounds(R.raw.black);
+        } else if (clickedBtnId == R.id.greenBtn) {
+            PlaySounds(R.raw.green);
+        }else if (clickedBtnId == R.id.purpleBtn){
+            PlaySounds(R.raw.purple);
+        }else if (clickedBtnId == R.id.yellowBtn){
+            PlaySounds(R.raw.yellow);
+        }
+    }
+
+    public void PlaySounds(int id){
+        MediaPlayer mediaPlayer = MediaPlayer.create(
+                this,
+                id
+        );
+        mediaPlayer.start();
+    }
 }
