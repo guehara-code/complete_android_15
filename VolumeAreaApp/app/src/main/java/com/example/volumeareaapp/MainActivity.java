6d4592f760fcb5418dcd8aa6d08cc75ed1fc1aa5
guehara-code <gil_uehara@yahoo.com.br>
2024-10-04 16:16:33 -0300

    Volume Area Calculator App - Main Activity created

--- VolumeAreaApp/app/src/main/java/com/example/volumeareaapp/MainActivity.java
+++ VolumeAreaApp/app/src/main/java/com/example/volumeareaapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.volumeareaapp;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+
+    ArrayList<Shape> shapeArrayList;
+
+    MyCustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        gridView = findViewById(R.id.gridView);
+
+        shapeArrayList = new ArrayList<>();
+
+        Shape s1 = new Shape(R.drawable.sphere, "Sphere");
+        Shape s2 = new Shape(R.drawable.cylinder, "Cylinder");
+        Shape s3 = new Shape(R.drawable.cube, "Cube");
+        Shape s4 = new Shape(R.drawable.prism, "Prism");
+
+        shapeArrayList.add(s1);
+        shapeArrayList.add(s2);
+        shapeArrayList.add(s3);
+        shapeArrayList.add(s4);
+
+        adapter = new MyCustomAdapter(shapeArrayList, getApplicationContext());
+
+        gridView.setAdapter(adapter);
+        gridView.setNumColumns(2);
+
     }
 }
