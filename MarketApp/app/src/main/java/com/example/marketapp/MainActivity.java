b7f2821a385c67df8d4cde99b7f9272efa6f0df2
guehara-code <gil_uehara@yahoo.com.br>
2024-10-05 16:26:34 -0300

    market app - MainActivity implemented

--- MarketApp/app/src/main/java/com/example/marketapp/MainActivity.java
+++ MarketApp/app/src/main/java/com/example/marketapp/MainActivity.java
@@ -7,8 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    List<Item> itemList;
+
+    MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        itemList = new ArrayList<>();
+        Item item1 = new Item(R.drawable.fruit, "Fruits", "Fresh Fruits from the Garden");
+        Item item2 = new Item(R.drawable.vegitables, "Vegetables", "Delicious Vegetables");
+        Item item3 = new Item(R.drawable.bread, "Bakery", "Bread, Wheat and Beans");
+        Item item4 = new Item(R.drawable.beverage, "Beverage", "Juice, Tea, Coffee and Soda");
+        Item item5 = new Item(R.drawable.milk, "Milk", "Milk, Shakes and Yogurt");
+        Item item6 = new Item (R.drawable.popcorn, "Snacks", "Pop Corn, Donut and Drinks");
+
+        itemList.add(item1);
+        itemList.add(item2);
+        itemList.add(item3);
+        itemList.add(item4);
+        itemList.add(item5);
+        itemList.add(item6);
+
+        myAdapter = new MyAdapter(itemList);
+        recyclerView.setAdapter(myAdapter);
     }
 }
