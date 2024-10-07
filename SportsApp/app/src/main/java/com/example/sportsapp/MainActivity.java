65a7738a5bef9d69c31d04fbb6e37d669ee9685a
guehara-code <gil_uehara@yahoo.com.br>
2024-10-07 20:10:23 -0300

    sports app - MainActivity class created

--- SportsApp/app/src/main/java/com/example/sportsapp/MainActivity.java
+++ SportsApp/app/src/main/java/com/example/sportsapp/MainActivity.java
@@ -7,8 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private List<Sport> sportList;
+
+    private CustomAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerView2);
+        sportList = new ArrayList<>();
+
+        Sport s1 = new Sport("Football", R.drawable.football);
+        Sport s2 = new Sport("Basketball", R.drawable.basketball);
+        Sport s3 = new Sport("VolleyBall", R.drawable.volley);
+        Sport s4 = new Sport("Tennis", R.drawable.tennis);
+        Sport s5 = new Sport("Ping Pong", R.drawable.ping);
+
+        sportList.add(s1);
+        sportList.add(s2);
+        sportList.add(s3);
+        sportList.add(s4);
+        sportList.add(s5);
+
+        myAdapter = new CustomAdapter(sportList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+
     }
+
+
 }
