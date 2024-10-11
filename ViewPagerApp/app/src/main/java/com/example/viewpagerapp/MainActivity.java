96988877e547243a516a2fd865295d35ba3f6256
guehara-code <gil_uehara@yahoo.com.br>
2024-10-11 20:42:12 -0300

    ViewPagerApp - fragments display implemented

--- ViewPagerApp/app/src/main/java/com/example/viewpagerapp/MainActivity.java
+++ ViewPagerApp/app/src/main/java/com/example/viewpagerapp/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewpager;
+    MyViewPagerAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        myAdapter = new MyViewPagerAdapter(
+                getSupportFragmentManager(),
+                getLifecycle()
+        );
+
+        myAdapter.addFragment(new Fragment1());
+        myAdapter.addFragment(new Fragment2());
+        myAdapter.addFragment(new Fragment3());
+
+        viewpager = findViewById(R.id.viewPager2);
+        viewpager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        viewpager.setAdapter(myAdapter);
+
     }
 }
