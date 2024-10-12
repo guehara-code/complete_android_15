54d9c56420224fb7bf6d63979ac6d135a334506b
guehara-code <gil_uehara@yahoo.com.br>
2024-10-12 08:06:35 -0300

    ViewPagerApp - fragments navigation with tabLayout implemented

--- ViewPagerApp/app/src/main/java/com/example/viewpagerapp/MainActivity.java
+++ ViewPagerApp/app/src/main/java/com/example/viewpagerapp/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.viewpagerapp;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 viewpager;
     MyViewPagerAdapter myAdapter;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tabLayout = findViewById(R.id.tablayout);
 
         myAdapter = new MyViewPagerAdapter(
                 getSupportFragmentManager(),
@@ -38,6 +45,17 @@ public class MainActivity extends AppCompatActivity {
         viewpager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
 
         viewpager.setAdapter(myAdapter);
+
+        new TabLayoutMediator(
+                tabLayout,
+                viewpager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Fragment" + (position + 1));
+                    }
+                }
+        ).attach();
 
     }
 }
