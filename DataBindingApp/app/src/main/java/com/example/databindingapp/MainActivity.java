7b43404026200524c5c5b4673ec6169ab97b04b0
guehara-code <gil_uehara@yahoo.com.br>
2024-10-13 16:41:41 -0300

    DataBindingApp - display data with data Binding implemented

--- DataBindingApp/app/src/main/java/com/example/databindingapp/MainActivity.java
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/MainActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.databindingapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Person p1 = new Person("Jack", "jack@gmail.com");
+        activityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main
+        );
+
+        activityMainBinding.setPerson(p1);
+
     }
 }
