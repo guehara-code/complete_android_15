099f9fb6e370db41678f60a6cbdce1c0d4060f60
guehara-code <gil_uehara@yahoo.com.br>
2024-10-15 08:32:05 -0300

    QuadraticEquationSolver - minor corrections implemented

--- QuadraticEquationSolver/app/src/main/java/com/example/quadraticequationsolver/MainActivity.java
+++ QuadraticEquationSolver/app/src/main/java/com/example/quadraticequationsolver/MainActivity.java
@@ -18,20 +18,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         mainBinding = DataBindingUtil.setContentView(
                 this,
                 R.layout.activity_main
         );
 
-        Equation equation = new Equation(mainBinding);
-        mainBinding.setEquation(equation);
+        MyEquation equation = new MyEquation(mainBinding);
+        mainBinding.setMyequation(equation);
     }
 }
+
