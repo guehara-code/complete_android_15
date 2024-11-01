c41ad47563c23fd8bd7310f0ef341344a94a7a33
guehara-code <gil_uehara@yahoo.com.br>
2024-11-01 17:12:55 -0300

    PagingApp - utils package created

---
+++ PaggingApp/app/src/main/java/com/example/paggingapp/util/Utils.java
@@ -0,0 +1,9 @@
+package com.example.paggingapp.util;
+
+import com.example.paggingapp.R;
+
+public class Utils {
+    public static String API_KEY = String.valueOf(R.string.api_key);
+    public static String BASE_URL = "https://api.themoviedb.org/3/";
+
+}
