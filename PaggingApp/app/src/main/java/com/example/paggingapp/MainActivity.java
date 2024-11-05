39ab8039b91f29f2acd82c1a051f02c9236da321
guehara-code <gil_uehara@yahoo.com.br>
2024-11-05 08:28:04 -0300

    PagingApp - Main activity class created

--- PaggingApp/app/src/main/java/com/example/paggingapp/MainActivity.java
+++ PaggingApp/app/src/main/java/com/example/paggingapp/MainActivity.java
@@ -1,24 +1,90 @@
 package com.example.paggingapp;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import com.bumptech.glide.RequestManager;
+import com.example.paggingapp.adapter.MoviesAdapter;
+import com.example.paggingapp.adapter.MoviesLoadStateAdapter;
+import com.example.paggingapp.databinding.ActivityMainBinding;
+import com.example.paggingapp.util.GridSpace;
+import com.example.paggingapp.util.MovieComparator;
+import com.example.paggingapp.util.Utils;
+import com.example.paggingapp.viewmodel.MovieViewModel;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+
+    MovieViewModel mainActivityViewModel;
+    ActivityMainBinding binding;
+    MoviesAdapter moviesAdapter;
+
+    @Inject
+    RequestManager requestManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        if(Utils.API_KEY == null || Utils.API_KEY.isEmpty()) {
+            Toast.makeText(this, "Error in API Key", Toast.LENGTH_SHORT).show();
+        }
+
+        moviesAdapter = new MoviesAdapter(new MovieComparator(), requestManager);
+
+        mainActivityViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
+
+        initRecyclerviewAndAdapter();
+
+        // subscribe to paging data
+        mainActivityViewModel.moviePagingDataFlowable.subscribe(moviePagingData -> {
+            moviesAdapter.submitData(getLifecycle(), moviePagingData);
+        });
+
+
+    }
+
+    private void initRecyclerviewAndAdapter() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+
+        binding.recyclerViewMovies.setLayoutManager(gridLayoutManager);
+
+        binding.recyclerViewMovies.addItemDecoration(new GridSpace(2, 20, true));
+
+        binding.recyclerViewMovies.setAdapter(
+                moviesAdapter.withLoadStateFooter(
+                        new MoviesLoadStateAdapter(view -> {
+                            moviesAdapter.retry();
+                        })
+                )
+        );
+
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return moviesAdapter.getItemViewType(position) == MoviesAdapter.LOADING_ITEM ? 1 : 2;
+            }
         });
     }
 }
