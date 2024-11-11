39b06701c4bd4fa84101565c5e23cc8616f6b1ae
guehara-code <gil_uehara@yahoo.com.br>
2024-11-11 09:01:47 -0300

    QuizApp - MainActivity class created

--- TheQuizApp/app/src/main/java/com/example/thequizapp/MainActivity.java
+++ TheQuizApp/app/src/main/java/com/example/thequizapp/MainActivity.java
@@ -7,8 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.thequizapp.databinding.ActivityMainBinding;
+import com.example.thequizapp.model.Question;
+import com.example.thequizapp.model.QuestionList;
+import com.example.thequizapp.viewmodel.QuizViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    QuizViewModel quizViewModel;
+    List<Question> questionList;
+
+    static int result = 0;
+    static int totalQuestions = 0;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_main
+        );
+
+        result = 0;
+        totalQuestions = 0;
+
+        quizViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
+    }
+
+    public void DisplayFirstQuestion() {
+        quizViewModel.getQuestionListLiveData().observe(
+                this,
+                new Observer<QuestionList>() {
+
+                    @Override
+                    public void onChanged(QuestionList questions) {
+                        questionList = questions;
+                        binding.txtQuestion.setText("Question 1: " + questions.get(0).getQuestion());
+                        binding.radio1.setText(questions.get(0).getOption1());
+                        binding.radio2.setText(questions.get(0).getOption2());
+                        binding.radio3.setText(questions.get(0).getOption3());
+                        binding.radio4.setText(questions.get(0).getOption4());
+                    }
+                }
+        );
     }
 }
