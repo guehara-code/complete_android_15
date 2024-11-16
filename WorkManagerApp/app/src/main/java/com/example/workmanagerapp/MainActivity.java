53e3b37289c64e1095950334195d9620f47185b7
guehara-code <gil_uehara@yahoo.com.br>
2024-11-16 14:43:56 -0300

    WorkManagerApp -  WorkManager status monitoring included

--- WorkManagerApp/app/src/main/java/com/example/workmanagerapp/MainActivity.java
+++ WorkManagerApp/app/src/main/java/com/example/workmanagerapp/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.workmanagerapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
@@ -39,5 +42,20 @@ public class MainActivity extends AppCompatActivity {
                         getApplicationContext()).enqueue(wr);
             }
         });
+
+        // Monitoring the status of work manager
+        WorkManager.getInstance(getApplicationContext())
+                .getWorkInfoByIdLiveData(wr.getId())
+                .observe(this,
+                        new Observer<WorkInfo>() {
+                            @Override
+                            public void onChanged(WorkInfo workInfo) {
+                                if(workInfo != null) {
+                                    Toast.makeText(MainActivity.this,
+                                            "Status: " + workInfo.getState().name(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
     }
 }
