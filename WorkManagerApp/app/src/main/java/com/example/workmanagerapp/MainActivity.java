cad50689ffb4cf77cb766d79e1a7772aa9e85e2c
guehara-code <gil_uehara@yahoo.com.br>
2024-11-17 08:16:24 -0300

    WorkManagerApp -  Contraints included in Worker

--- WorkManagerApp/app/src/main/java/com/example/workmanagerapp/MainActivity.java
+++ WorkManagerApp/app/src/main/java/com/example/workmanagerapp/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
@@ -33,7 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
         btn = findViewById(R.id.btn);
 
-        WorkRequest wr = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+        //Constraints
+
+        Constraints constraints = new Constraints
+                .Builder()
+                .setRequiresCharging(true)
+                .build();
+
+        // Making use of worker
+
+        WorkRequest wr = new OneTimeWorkRequest
+                .Builder(MyWorker.class)
+                .setConstraints(constraints)
+                .build();
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
