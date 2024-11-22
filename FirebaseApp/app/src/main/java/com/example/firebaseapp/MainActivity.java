5a7b30e9e93412d4e338ee57986e7abaf4f4c0a0
guehara-code <gil_uehara@yahoo.com.br>
2024-11-22 08:24:23 -0300

    FirebaseApp - data listener implemented

--- FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.firebaseapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -12,9 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +39,23 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("messages");
 
         myRef.setValue("Hello from our Course!");
-        
+
+        TextView textView = findViewById(R.id.textview);
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String newValue = snapshot.getValue(String.class);
+                textView.setText(newValue);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
