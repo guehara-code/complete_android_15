3feebe09ec89f437fee92008723e985afb4ec99a
guehara-code <gil_uehara@yahoo.com.br>
2024-11-23 08:21:57 -0300

    FirebaseApp - data binding included

--- FirebaseApp/app/src/main/java/com/example/firebaseapp/User.java
+++ FirebaseApp/app/src/main/java/com/example/firebaseapp/User.java
@@ -1,6 +1,9 @@
 package com.example.firebaseapp;
 
-public class User {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class User extends BaseObservable {
 
     String username;
     String phoneNumber;
@@ -15,27 +18,33 @@ public class User {
     public User() {
     }
 
+    @Bindable
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+        notifyPropertyChanged(BR.username);
     }
 
+    @Bindable
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+        notifyPropertyChanged(BR.phoneNumber);
     }
 
+    @Bindable
     public String getGroupUser() {
         return groupUser;
     }
 
     public void setGroupUser(String groupUser) {
         this.groupUser = groupUser;
+        notifyPropertyChanged(BR.groupUser);
     }
 }
