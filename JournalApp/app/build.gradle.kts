0e66e06703aa0c0033bf17e7adb60533ced33e08
guehara-code <gil_uehara@yahoo.com.br>
2024-11-27 17:09:03 -0300

    JournalApp - dependencies inclusion

--- JournalApp/app/build.gradle.kts
+++ JournalApp/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,4 +41,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storate")
+
+    implementation(libs.glide)
+    implementation(libs.compiler)
 }
