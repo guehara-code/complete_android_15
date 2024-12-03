b4bb169d1267ffa11de4ed407468a0043fe4bf9b
guehara-code <gil_uehara@yahoo.com.br>
2024-12-03 08:27:03 -0300

    JournalApp - sign up main activity implemented

--- JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.java
+++ JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.java
@@ -1,14 +1,35 @@
 package com.example.journalapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText password_create, username_create, email_create;
+    Button createBTN;
+
+    // Firebase Auth
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    // Firebase Connection
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +41,32 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        createBTN = findViewById(R.id.acc_signUp_btn);
+        password_create = findViewById(R.id.password_create);
+        email_create = findViewById(R.id.email_create);
+
+        // Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Listening for changes in the authentication
+        // state and responds accordingly when the
+        // state changes
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                // Check the user if logged in or not
+                if (currentUser != null) {
+                    // user already logged in
+                    
+                } else {
+                    // the user signed out
+                }
+
+            }
+        }
+
     }
 }
