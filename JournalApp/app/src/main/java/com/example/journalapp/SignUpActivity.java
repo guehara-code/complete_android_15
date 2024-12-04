3cc00c77606819d50059e4f20829235df373d555
guehara-code <gil_uehara@yahoo.com.br>
2024-12-04 08:10:17 -0300

    JournalApp - Create user function implemented

--- JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.java
+++ JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.java
@@ -1,8 +1,11 @@
 package com.example.journalapp;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -11,6 +14,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -60,13 +66,56 @@ public class SignUpActivity extends AppCompatActivity {
                 // Check the user if logged in or not
                 if (currentUser != null) {
                     // user already logged in
-                    
+
+
                 } else {
                     // the user signed out
                 }
 
             }
-        }
+        };
+
+        createBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(email_create.getText().toString())
+                        && !TextUtils.isEmpty(username_create.getText().toString())
+                        && !TextUtils.isEmpty(password_create.getText().toString())) {
+                    String email = email_create.getText().toString().trim();
+                    String pass = password_create.getText().toString().trim();
+                    String username = username_create.getText().toString().trim();
+
+                    CreateUserEmailAccount(email, pass, username);
+                } else {
+                    Toast.makeText(SignUpActivity.this,
+                            "No Empty Fields are Allowed",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
+
+    private void CreateUserEmailAccount(
+            String email,
+            String pass,
+            String username
+    ) {
+        if (!TextUtils.isEmpty(email)
+                && !TextUtils.isEmpty(pass)
+                && !TextUtils.isEmpty(username)) {
+            firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUpActivity.this,
+                                        "Account is created succesfully",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
 }
