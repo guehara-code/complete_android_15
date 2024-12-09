0fa83131d4fb31911e27d492964280a4a3d19e66
guehara-code <gil_uehara@yahoo.com.br>
2024-12-09 08:26:09 -0300

    JournalApp - AddJournal activity implemented

--- JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.java
+++ JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.java
@@ -1,6 +1,11 @@
 package com.example.journalapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class AddJournalActivity extends AppCompatActivity {
+
+    // Widgets
+    private Button saveButton;
+    private ImageView addPhotoBtn;
+    private ProgressBar progressBar;
+    private EditText titleEditText;
+    private EditText thoughtsEditText;
+    private ImageView imageView;
+
+    // Firebase
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Journal");
+
+    // Firebase (Storage)
+    private StorageReference storageReference;
+
+    // Firebase Auth
+    private String currentUserId;
+    private String currentUserName;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +55,41 @@ public class AddJournalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        progressBar = findViewById(R.id.post_progressBar);
+        titleEditText = findViewById(R.id.post_title_et);
+        thoughtsEditText = findViewById(R.id.post_description_et);
+        imageView = findViewById(R.id.post_imageView);
+        saveButton = findViewById(R.id.post_save_journal_button);
+        addPhotoBtn = findViewById(R.id.postCameraButton);
+
+        progressBar.setVisibility(ImageView.INVISIBLE);
+
+        // Firebase
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        // Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Getting the Current User
+        if (user != null) {
+            currentUserId = user.getUid();
+            currentUserName = user.getDisplayName();
+        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveJournal();
+            }
+        });
+
+        addPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
