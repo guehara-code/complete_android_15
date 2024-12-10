cc1e27e560ef97482acf29ca373ffdd5886b847e
guehara-code <gil_uehara@yahoo.com.br>
2024-12-10 08:36:30 -0300

    JournalApp - upload journal to firebase implemented

--- JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.java
+++ JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.java
@@ -1,12 +1,15 @@
 package com.example.journalapp;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.ActivityViewModelLazyKt;
 import androidx.activity.EdgeToEdge;
@@ -14,17 +17,25 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Date;
 
 public class AddJournalActivity extends AppCompatActivity {
 
@@ -52,6 +63,7 @@ public class AddJournalActivity extends AppCompatActivity {
 
     // Using Activity Result Launcher
     ActivityResultLauncher<String> mTakePhoto;
+    Uri imageUri;
 
 
     @Override
@@ -89,7 +101,7 @@ public class AddJournalActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //SaveJournal();
+                SaveJournal();
             }
         });
 
@@ -99,6 +111,8 @@ public class AddJournalActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(Uri result) {
                         imageView.setImageURI(result);
+
+                        imageUri = result;
                     }
                 }
         );
@@ -111,5 +125,79 @@ public class AddJournalActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void SaveJournal() {
+
+        String title = titleEditText.getText().toString().trim();
+        String thoughts = thoughtsEditText.getText().toString().trim();
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(thoughts)
+        && imageUri != null) {
+            // the saving path of the images in Firebase Storage:
+            // ......./journal_images/out_image_YYYYMMDDHHMM.png
+            final StorageReference filePath = storageReference
+                    .child("journal_images")
+                    .child("my_image_" + Timestamp.now().getSeconds());
+
+            // Uploading the image
+            filePath.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String imageUrl = uri.toString();
+
+                                    // Creating a Journal Object
+                                    Journal journal = new Journal();
+                                    journal.setTitle(title);
+                                    journal.setThoughts(thoughts);
+                                    journal.setImageUrl(imageUrl);
+
+                                    journal.setTimeAdded(new Timestamp(new Date()));
+                                    journal.setUserName(currentUserName);
+                                    journal.setUserId(currentUserId);
+
+                                    collectionReference.add(journal)
+                                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                @Override
+                                                public void onSuccess(DocumentReference documentReference) {
+                                                    progressBar.setVisibility(View.INVISIBLE);
+
+                                                    Intent i  = new Intent(AddJournalActivity.this, JournalListActivity.class);
+                                                    startActivity(i);
+                                                    finish();
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(AddJournalActivity.this,
+                                                            "Failed : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(AddJournalActivity.this,
+                                                            "Failed : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                }
+                            });
+                        }
+                    });
+        } else {
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = firebaseAuth.getCurrentUser();
     }
 }
