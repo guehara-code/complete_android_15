d65036920610b65f4085464dac12de80f704b574
guehara-code <gil_uehara@yahoo.com.br>
2024-12-12 08:22:40 -0300

    ChatApp - Repository class created

---
+++ ChatApp/app/src/main/java/com/example/chatapp/repository/Repository.java
@@ -0,0 +1,31 @@
+package com.example.chatapp.repository;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import com.example.chatapp.views.GroupsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Repository {
+
+    // It acts as a bridge between the ViewModel and the data Source
+
+    public void firebaseAnonymousAuth(Context context) {
+        FirebaseAuth.getInstance().signInAnonymously()
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent i  = new Intent(context, GroupsActivity.class);
+                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(i);
+                        }
+                    }
+                });
+    }
+}
