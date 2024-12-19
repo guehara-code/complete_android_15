395273a815edf633e13d7b641e4d8defcd8abb41
guehara-code <gil_uehara@yahoo.com.br>
2024-12-19 08:28:15 -0300

    ChatApp - GroupsActivity layout created

--- ChatApp/app/src/main/java/com/example/chatapp/views/GroupsActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/views/GroupsActivity.java
@@ -7,10 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityGroupsBinding;
+import com.example.chatapp.model.ChatGroup;
+import com.example.chatapp.viewmodel.MyViewModel;
+import com.example.chatapp.views.adapters.GroupAdapter;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupsActivity extends AppCompatActivity {
+
+    private ArrayList<ChatGroup> chatGroupArrayList;
+
+    private RecyclerView recyclerView;
+    private GroupAdapter groupAdapter;
+    private ActivityGroupsBinding binding;
+    private MyViewModel myViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +43,33 @@ public class GroupsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_groups
+        );
+
+        // Define the ViewModel
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        // RecyclerView With Data Binding
+        recyclerView = binding.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Setup an observer to listen for changes in a "Live data" object
+        myViewModel.getGroupList().observe(this, new Observer<List<ChatGroup>>() {
+            @Override
+            public void onChanged(List<ChatGroup> chatGroups) {
+                chatGroupArrayList = new ArrayList<>();
+                chatGroupArrayList.addAll(chatGroups);
+
+                groupAdapter = new GroupAdapter(chatGroupArrayList);
+
+                recyclerView.setAdapter(groupAdapter);
+                groupAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
 }
