b70a89750edc0dd38daab0d17772751f81dd8b1c
guehara-code <gil_uehara@yahoo.com.br>
2024-12-22 08:37:20 -0300

    ChatApp - show add group dialog implementation

--- ChatApp/app/src/main/java/com/example/chatapp/views/GroupsActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/views/GroupsActivity.java
@@ -1,6 +1,13 @@
 package com.example.chatapp.views;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +39,9 @@ public class GroupsActivity extends AppCompatActivity {
     private GroupAdapter groupAdapter;
     private ActivityGroupsBinding binding;
     private MyViewModel myViewModel;
+
+    // Dialog
+    private Dialog chatGroupDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +77,31 @@ public class GroupsActivity extends AppCompatActivity {
 
                 recyclerView.setAdapter(groupAdapter);
                 groupAdapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+
+    public void showDialog() {
+        chatGroupDialog = new Dialog(this);
+        chatGroupDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        View view = LayoutInflater.from(this)
+                .inflate(R.layout.dialog_layout,
+                        null);
+
+        chatGroupDialog.setContentView(view);
+        chatGroupDialog.show();
+
+        Button submit = view.findViewById(R.id.submit_btn);
+        EditText edt = view.findViewById(R.id.chat_group_edt);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String groupName = edt.getText().toString();
+                Toast.makeText(GroupsActivity.this,
+                        "Your Chat Group: " + groupName, Toast.LENGTH_SHORT).show();
             }
         });
 
