f20b55f65fae46a223761d8b9be4f8aeee7e0532
guehara-code <gil_uehara@yahoo.com.br>
2024-12-26 08:17:58 -0300

    Image2TextApp - main activity implemented

--- Image2TextApp/app/src/main/java/com/example/image2textapp/MainActivity.java
+++ Image2TextApp/app/src/main/java/com/example/image2textapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.image2textapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button captureBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        captureBtn = findViewById(R.id.btn);
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Activity_scanner.class);
+                startActivity(i);
+            }
+        });
+
     }
+
+
 }
