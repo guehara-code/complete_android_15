7d56f37fb9f12e441f08db651c91477e438571ca
guehara-code <gil_uehara@yahoo.com.br>
2024-12-27 08:15:54 -0300

    Image2TextApp - scanner activity implemented

--- Image2TextApp/app/src/main/java/com/example/image2textapp/Activity_scanner.java
+++ Image2TextApp/app/src/main/java/com/example/image2textapp/Activity_scanner.java
@@ -1,14 +1,32 @@
 package com.example.image2textapp;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Activity_scanner extends AppCompatActivity {
+
+    // Widgets
+    private ImageView captureIV;
+    private TextView resultIV;
+    private Button snapBtn, detectBtn;
+    private Bitmap imageBitmap;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,51 @@ public class Activity_scanner extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        captureIV = findViewById(R.id.idIVCaptureImage);
+        resultIV = findViewById(R.id.idTVDetectedText);
+        snapBtn = findViewById(R.id.idButtonSnap);
+        detectBtn = findViewById(R.id.idButtonDetect);
+
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DetectText();
+            }
+        });
+
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (CheckPermission()) {
+                    CaptureImage();
+                } else {
+                    RequestPermission();
+                }
+            }
+        });
+
+    }
+
+    // Permission methods
+    private boolean CheckPermission() {
+        int cameraPermission = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA_SERVICE);
+        return cameraPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void RequestPermission() {
+        int PERMISSION_CODE = 200;
+        ActivityCompat.requestPermissions(this, new String[]{
+                android.Manifest.permission.CAMERA
+        }, PERMISSION_CODE);
+    }
+
+    private void CaptureImage() {
+
+    }
+
+
+    private void DetectText() {
+
     }
 }
