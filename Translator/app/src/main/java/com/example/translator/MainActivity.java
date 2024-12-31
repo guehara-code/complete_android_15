dff70954790f16633d5db7afdc86cfe6f857908f
guehara-code <gil_uehara@yahoo.com.br>
2024-12-31 08:17:56 -0300

    Translator - main activity widgets initialization created

--- Translator/app/src/main/java/com/example/translator/MainActivity.java
+++ Translator/app/src/main/java/com/example/translator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.translator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Widgets
+
+    private Spinner fromSpinner, toSpinner;
+    private EditText sourceEdt;
+    private Button btn;
+    private TextView translatedTV;
+
+    // Source Array of Strings - Spinners` data
+    String[] fromLanguages = {
+            "from", "English", "Afrikaans", "Arabic", "Belarusian", "Bengali", "Catalan", "Hindi", "Urdu"
+    };
+
+    String[] toLanguages = {
+            "to", "English", "Afrikaans", "Arabic", "Belarusian", "Bengali", "Catalan", "Hindi", "Urdu"
+    };
+
+    // Permissions
+    private static final int REQUEST_CODE = 1;
+
+    int languageCode, fromLanguage, toLanguage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +45,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        fromSpinner = findViewById(R.id.idFromSpinner);
+        toSpinner = findViewById(R.id.idToSpinner);
+        sourceEdt = findViewById(R.id.idEdtSource);
+        btn = findViewById(R.id.button);
+        translatedTV = findViewById(R.id.idTvTranslatedTV);
+
     }
+
+
+
 }
