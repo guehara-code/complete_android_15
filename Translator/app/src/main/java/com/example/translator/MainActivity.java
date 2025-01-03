00319329d571b29a30135966f380f81951d12d42
guehara-code <gil_uehara@yahoo.com.br>
2025-01-03 08:04:59 -0300

    Translator - main activity translation implementation

--- Translator/app/src/main/java/com/example/translator/MainActivity.java
+++ Translator/app/src/main/java/com/example/translator/MainActivity.java
@@ -8,14 +8,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.TranslatorOptions;
+import com.google.mlkit.nl.translate.Translator;
 
 import java.sql.Array;
 
@@ -96,8 +104,67 @@ public class MainActivity extends AppCompatActivity {
                 R.layout.spinner_item, toLanguages);
         toAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         toSpinner.setAdapter(toAdapter);
+        
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                translatedTV.setText("");
+                if(sourceEdt.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter your text", Toast.LENGTH_SHORT).show();
+                } else if(fromLanguageCode.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please select source language", Toast.LENGTH_SHORT).show();
+                } else if(toLanguageCode.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please select the target language", Toast.LENGTH_SHORT).show();
+                } else {
+                    TranslateText(fromLanguageCode, toLanguageCode, sourceEdt.getText().toString());
+                }
+
+            }
+        });
 
 
+    }
+
+    private void TranslateText(String fromLanguageCode, String toLanguageCode, String src) {
+
+        translatedTV.setText("Downloading language model");
+
+        try {
+            TranslatorOptions options = new TranslatorOptions.Builder().setSourceLanguage(fromLanguageCode)
+                    .setTargetLanguage(toLanguageCode).build();
+
+            Translator translator = Translation.getClient(options);
+
+            DownloadConditions conditions = new DownloadConditions.Builder().build();
+
+            translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    translatedTV.setText("Translation...");
+                    translator.translate(src).addOnSuccessListener(new OnSuccessListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            translatedTV.setText(s);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this,
+                                    "Fail to translate", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,
+                            "Fail to download the language", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private String GetLanguageCode(String language) {
