ecf05d669f69f9a8351e933f07fba1d112f44900
guehara-code <gil_uehara@yahoo.com.br>
2025-01-04 20:41:52 -0300

    OCR - dependencies added

--- OCR/app/build.gradle.kts
+++ OCR/app/build.gradle.kts
@@ -40,4 +40,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.1")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
 }
