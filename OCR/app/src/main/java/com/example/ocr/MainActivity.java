54293280f3cd1c7069b85cf48e890760b27bde25
guehara-code <gil_uehara@yahoo.com.br>
2025-01-06 08:07:21 -0300

    OCR - Main activity implemented

--- OCR/app/src/main/java/com/example/ocr/MainActivity.java
+++ OCR/app/src/main/java/com/example/ocr/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ocr;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Widgets
+    ImageView imageView;
+    TextView textView;
+    Button imageBTN, speechBTN;
+
+    // Variables
+    InputImage inputImage;
+    TextRecognizer recognizer;
+    TextToSpeech textToSpeech;
+    public Bitmap textImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +44,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+        imageBTN = findViewById(R.id.choose_image);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.text);
+        speechBTN = findViewById(R.id.speech);
+
+        imageBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenGallery();
+            }
+        });
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        speechBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(textView.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+    }
+
+    private void OpenGallery() {
+
+
     }
 }
