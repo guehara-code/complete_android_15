8ad79e5357b0c114634aaf41414430ef50624d75
guehara-code <gil_uehara@yahoo.com.br>
2025-01-10 08:47:52 -0300

    QRScanner - dependencies and permissions added

--- QRScanner/app/build.gradle.kts
+++ QRScanner/app/build.gradle.kts
@@ -40,4 +40,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Bar Code Scanning - without Google Play Service
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+
+    val camerax_version = "1.2.2"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation ("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation ("androidx.camera:camera-video:${camerax_version}")
+
+    implementation ("androidx.camera:camera-view:${camerax_version}")
+    implementation ("androidx.camera:camera-extensions:${camerax_version}")
+
 }
--- QRScanner/settings.gradle.kts
+++ QRScanner/settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "QRScanner"
 include(":app")
- 
