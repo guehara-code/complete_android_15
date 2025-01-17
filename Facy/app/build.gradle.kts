7ceeec5aa3f40ed8a8ef1562647221a91d4f7703
guehara-code <gil_uehara@yahoo.com.br>
2025-01-17 08:21:21 -0300

    Facy app - dependencies and settings included

--- Facy/app/build.gradle.kts
+++ Facy/app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.android.gms:play-services-mlkit-face-detection:17.1.0")
 }
