e27ef624a7707ad5075eb2a5aadd1a52286cac7c
guehara-code <gil_uehara@yahoo.com.br>
2025-01-22 15:03:49 -0300

    Facy app - detection funcion implemented

--- Facy/app/src/main/java/com/example/facy/MainActivity.java
+++ Facy/app/src/main/java/com/example/facy/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.facy;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -11,15 +12,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
 import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -80,5 +90,82 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void FaceDetectionProcess(Bitmap bitmap) {
+
+        textView.setText("Processing Image ... ");
+
+        final StringBuilder builder = new StringBuilder();
+
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+        FaceDetectorOptions highAccuracyOpt = new FaceDetectorOptions.Builder()
+                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                .enableTracking().build();
+
+        FaceDetector detector = FaceDetection.getClient(highAccuracyOpt);
+
+        Task<List<Face>> result = detector.process(image);
+        result.addOnSuccessListener(new OnSuccessListener<List<Face>>() {
+            @Override
+            public void onSuccess(List<Face> faces) {
+                if (faces.size() != 0) {
+                    if (faces.size() == 1) {
+                        builder.append(faces.size() + " Face Detected\n\n");
+                    } else if (faces.size() > 1) {
+                        builder.append(faces.size() + " Faces Detected\n\n");
+                    }
+                }
+
+                for (Face face : faces) {
+                    int id = face.getTrackingId();
+                    float rotY = face.getHeadEulerAngleY();
+                    float rotZ = face.getHeadEulerAngleZ();
+
+                    builder.append("1. Face Tracking ID [" + id + "]\n");
+                    builder.append("2. Head Rotation to Right [" + String.format("%.2f", rotY) + " deg ]\n");
+                    builder.append("3. Head Tilted Sideways [" + String.format("%.2f", rotZ) + " deg ]\n");
+
+                    // Smiling Probability
+
+                    if (face.getSmilingProbability() > 0) {
+                        float smilingProbability = face.getSmilingProbability();
+                        builder.append("4. Smiling Probability [" + String.format("%.2f", smilingProbability) + "]\n");
+                    }
+
+                    // Left Eye Opened Probability
+
+                    if (face.getLeftEyeOpenProbability() > 0) {
+                        float leftEye = face.getLeftEyeOpenProbability();
+                        builder.append("4. Left Eye Opened Probability [" + String.format("%.2f", leftEye) + "]\n");
+                    }
+
+                    // Right Eye Opened Probability
+
+                    if (face.getRightEyeOpenProbability() > 0) {
+                        float rightEye = face.getRightEyeOpenProbability();
+                        builder.append("4. Right Eye Opened Probability [" + String.format("%.2f", rightEye) + "]\n");
+                    }
+
+                    builder.append("\n");
+                }
+
+                ShowDetection("Face Detection", builder, true);
+            }
+        });
+        result.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                StringBuilder buider1 = new StringBuilder();
+                buider1.append("Sorry!! There is some error!");
+                ShowDetection("Face Detection", builder, false);
+            }
+        });
+
+    }
+
+    public void ShowDetection(final String title, final StringBuilder buider, boolean success) {
+        
     }
 }
