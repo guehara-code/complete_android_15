1dd01c3d7e05a1ec530781d8cbe00c3c348f3de8
guehara-code <gil_uehara@yahoo.com.br>
2025-02-13 08:12:11 -0300

    KotlinBasics - Variable demonstration

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/HelloKotlin.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/HelloKotlin.kt
@@ -1,5 +1,12 @@
 package com.example.kotlinbasics
 
 fun main() {
-    println("Hello My Friends")
+
+    // Mutable variable
+    var myAge : Int = 25
+
+    // Immutable variable
+    val pi : Double = 3.14159
+
+    println("My age is: " + myAge)
 }
