036a4e085f4243fb3119b295eb8730f92c32da5a
guehara-code <gil_uehara@yahoo.com.br>
2025-03-19 08:11:57 -0300

    KotlinExercises - exercise 6

---
+++ KotlinExercises/app/src/main/java/com/example/kotlinexercises/Exercise6.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinexercises
+
+fun main() {
+
+    var arr1 = arrayOf(1, 3, -5, 4)
+    var arr2 = arrayOf(1, 4, -5, -2)
+    var result = ""
+
+    for(i in arr1.withIndex()) {
+        result += (arr1[i.index] * arr2[i.index])
+        result += " "
+    }
+
+    print("Result: " + result)
+}
