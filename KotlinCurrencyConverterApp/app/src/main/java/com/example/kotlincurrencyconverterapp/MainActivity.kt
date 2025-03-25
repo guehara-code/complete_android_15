212e29b785cc1f030c20f482582c3061edba92cd
guehara-code <gil_uehara@yahoo.com.br>
2025-03-25 08:24:25 -0300

    KotlinCurrencyConverterApp - variables creation and intialization

--- KotlinCurrencyConverterApp/app/src/main/java/com/example/kotlincurrencyconverterapp/MainActivity.kt
+++ KotlinCurrencyConverterApp/app/src/main/java/com/example/kotlincurrencyconverterapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.kotlincurrencyconverterapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // Declaring variables (widgets)
+
+    lateinit var titleTextView: TextView
+    lateinit var resultTextView: TextView
+    lateinit var editText: EditText
+    lateinit var convertButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Initializing widgets
+        titleTextView = findViewById(R.id.textView)
+        resultTextView = findViewById(R.id.resultText)
+        editText = findViewById(R.id.editText)
+        convertButton = findViewById(R.id.convertBTN)
+
     }
 }
