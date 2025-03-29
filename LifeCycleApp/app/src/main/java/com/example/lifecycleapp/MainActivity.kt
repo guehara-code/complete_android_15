51dd422abf18c936a924377c7a2d3bc153b7b64c
guehara-code <gil_uehara@yahoo.com.br>
2025-03-29 07:51:24 -0300

    LifeCycleApp - passing data to second activity implemented

--- LifeCycleApp/app/src/main/java/com/example/lifecycleapp/MainActivity.kt
+++ LifeCycleApp/app/src/main/java/com/example/lifecycleapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.lifecycleapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        myButton = findViewById(R.id.goToNextBtn)
+        myButton.setOnClickListener {
+            val explicitIntent = Intent(this, SecondActivity::class.java)
+
+            explicitIntent.putExtra("myname", "Jack")
+            startActivity(explicitIntent)
+        }
+
+
     }
 }
--- LifeCycleApp/app/src/main/java/com/example/lifecycleapp/SecondActivity.kt
+++ LifeCycleApp/app/src/main/java/com/example/lifecycleapp/SecondActivity.kt
@@ -1,12 +1,16 @@
 package com.example.lifecycleapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var titleTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        titleTextView = findViewById(R.id.myTextView)
+
+        // Receive data from Intent
+        val receivedData = intent.getStringExtra("myname")
+
+        titleTextView.text = "Welcome $receivedData to second activity"
     }
 }
