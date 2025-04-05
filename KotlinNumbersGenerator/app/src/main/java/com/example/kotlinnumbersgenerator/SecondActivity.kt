4cdc491575d07861097f4720ebb94e75c01a69d5
guehara-code <gil_uehara@yahoo.com.br>
2025-04-05 09:22:06 -0300

    KotlinNumbersGenerator - second activity initialization implemented

--- KotlinNumbersGenerator/app/src/main/java/com/example/kotlinnumbersgenerator/SecondActivity.kt
+++ KotlinNumbersGenerator/app/src/main/java/com/example/kotlinnumbersgenerator/SecondActivity.kt
@@ -1,12 +1,19 @@
 package com.example.kotlinnumbersgenerator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var textViewTitle: TextView
+    lateinit var textGeneratedNumbers: TextView
+    lateinit var shareButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,9 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        textViewTitle = findViewById(R.id.textView2)
+        textGeneratedNumbers = findViewById(R.id.resultTextView)
+        shareButton = findViewById(R.id.shareBtn)
     }
 }
