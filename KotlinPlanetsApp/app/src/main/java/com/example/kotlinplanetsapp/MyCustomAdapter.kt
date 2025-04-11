ff3b3886668fc205aa3a104c4840b517d8657f5b
guehara-code <gil_uehara@yahoo.com.br>
2025-04-11 07:59:38 -0300

    KotlinPlanetsApp - GetView method created

--- KotlinPlanetsApp/app/src/main/java/com/example/kotlinplanetsapp/MyCustomAdapter.kt
+++ KotlinPlanetsApp/app/src/main/java/com/example/kotlinplanetsapp/MyCustomAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinplanetsapp
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
 class MyCustomAdapter(val context: Context, val planets: List<Planet>): BaseAdapter {
     override fun getCount(): Int {
@@ -19,7 +22,25 @@ class MyCustomAdapter(val context: Context, val planets: List<Planet>): BaseAdap
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val inflater = context.getSystemService(
+            Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View
+        if(convertView == null) {
+            view = inflater.inflate(R.layout.item_list_layout,
+                parent, false)
+        } else {
+            view = convertView
+        }
+        val item = getItem(position) as Planet
+        val titleTextView = view.findViewById<TextView>(R.id.planet_name)
+        val moonCountTextView = view.findViewById<TextView>(R.id.moon_count_text)
+        val moon_image = view.findViewById<ImageView>(R.id.imageView)
 
+        titleTextView.text = item.title
+        moonCountTextView.text = item.moonCount
+        moon_image.setImageResource(item.imagePlanet)
+
+        return view
     }
 
 
