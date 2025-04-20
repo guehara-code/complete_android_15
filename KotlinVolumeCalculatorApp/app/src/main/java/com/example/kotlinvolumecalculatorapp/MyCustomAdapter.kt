b693adbdcfb4369612c74f9919d60f109b6b7731
guehara-code <gil_uehara@yahoo.com.br>
2025-04-19 21:11:36 -0300

    KotlinVolumeCalculatorApp - viewHolder and getView included

--- KotlinVolumeCalculatorApp/app/src/main/java/com/example/kotlinvolumecalculatorapp/MyCustomAdapter.kt
+++ KotlinVolumeCalculatorApp/app/src/main/java/com/example/kotlinvolumecalculatorapp/MyCustomAdapter.kt
@@ -1,6 +1,9 @@
 package com.example.kotlinvolumecalculatorapp
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,7 +13,34 @@ import android.widget.TextView
 class MyCustomAdapter(context: Context, private val gridItems: List<Shape>)
     : ArrayAdapter<Shape>(context, 0, gridItems) {
 
-        private class ViewHolder {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var itemView = convertView
+        val holder: ViewHolder
+
+        if(convertView == null) {
+            itemView = LayoutInflater.from(context)
+                .inflate(R.layout.grid_item_layout, parent, false)
+
+            holder = ViewHolder()
+            holder.imageView = itemView.findViewById(R.id.imageView)
+            holder.textView = itemView.findViewById(R.id.textView)
+
+            itemView.tag = holder
+        } else {
+            holder = itemView?.tag as ViewHolder
+        }
+
+        val currentItem = gridItems[position]
+        holder.imageView.setImageResource(currentItem.shapeImg)
+        holder.textView.text = currentItem.shapeName
+
+        return itemView!!
+
+
+    }
+
+    private class ViewHolder {
             lateinit var imageView: ImageView
             lateinit var textView: TextView
         }
