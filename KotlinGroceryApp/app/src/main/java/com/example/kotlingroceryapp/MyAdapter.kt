79c4fc6e8e36a53d25ac80494988cff87e01dee5
guehara-code <gil_uehara@yahoo.com.br>
2025-04-29 08:28:42 -0300

    KotlinGroceryApp - ViewHoder class items initialized

--- KotlinGroceryApp/app/src/main/java/com/example/kotlingroceryapp/MyAdapter.kt
+++ KotlinGroceryApp/app/src/main/java/com/example/kotlingroceryapp/MyAdapter.kt
@@ -1,10 +1,25 @@
 package com.example.kotlingroceryapp
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MyAdapter(val itemsList: ArrayList<ItemModel>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(itemView: View)
-        : RecyclerView.ViewHolder(itemView)
+        : RecyclerView.ViewHolder(itemView) {
+
+            var itemImg: ImageView
+            var itemTitle: TextView
+            var itemDesc: TextView
+
+            init {
+                itemImg = itemView.findViewById(R.id.imageView)
+                itemTitle = itemView.findViewById(R.id.title_txt)
+                itemDesc = itemView.findViewById(R.id.description_text)
+            }
+        }
+
+
 }
