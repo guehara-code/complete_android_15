5c54fa09a199f10293ca280671c908b0f67b8923
guehara-code <gil_uehara@yahoo.com.br>
2025-05-06 08:02:07 -0300

    KotlinSportsApp - main activity implemented

--- KotlinSportsApp/app/src/main/java/com/example/kotlinsportsapp/MainActivity.kt
+++ KotlinSportsApp/app/src/main/java/com/example/kotlinsportsapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,33 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // RecyclerView
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
+        // Data source: List of SportsModel objects
+        var sportsList: ArrayList<SportModel> = ArrayList()
+
+        val sport1 = SportModel(R.drawable.football , "Football")
+        val sport2 = SportModel(R.drawable.basketball , "Basketball")
+        val sport3 = SportModel(R.drawable.ping , "Ping Pong")
+        val sport4 = SportModel(R.drawable.tennis , "Tennis")
+        val sport5 = SportModel(R.drawable.volley , "VolleyBall")
+
+        sportsList.add(sport1)
+        sportsList.add(sport2)
+        sportsList.add(sport3)
+        sportsList.add(sport4)
+        sportsList.add(sport5)
+
+        // Adapter
+        val adapter = SportsAdapter(sportsList)
+        recyclerView.adapter = adapter
+
     }
 }
