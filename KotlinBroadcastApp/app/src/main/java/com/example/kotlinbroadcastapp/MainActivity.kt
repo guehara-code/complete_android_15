943d77dce0334f91a2c71c20b927283ea54deb1c
guehara-code <gil_uehara@yahoo.com.br>
2025-05-13 08:26:31 -0300

    KotlinBroadcastApp - main activity implemented

--- KotlinBroadcastApp/app/src/main/java/com/example/kotlinbroadcastapp/MainActivity.kt
+++ KotlinBroadcastApp/app/src/main/java/com/example/kotlinbroadcastapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinbroadcastapp
 
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var airplaneModeReceiver: AirplaneModeReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Register Dynamically the Broadcast Receiver
+        var intentFilter: IntentFilter =
+            IntentFilter("android.intent.action.AIRPLANE_MODE")
+
+
+        airplaneModeReceiver = AirplaneModeReceiver()
+        registerReceiver(airplaneModeReceiver, intentFilter)
+
+
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(airplaneModeReceiver)
+        super.onDestroy()
     }
 }
