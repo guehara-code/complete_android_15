ef81d091fbbdc5f2761951871d952d250ae1b4da
guehara-code <gil_uehara@yahoo.com.br>
2025-05-15 07:59:23 -0300

    KotlinFragmentsApp - fragments layout location corrected

--- KotlinFragmentsApp/app/src/main/java/com/example/kotlinfragmentsapp/FragmentOne.kt
+++ KotlinFragmentsApp/app/src/main/java/com/example/kotlinfragmentsapp/FragmentOne.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinbroadcastapp
+package com.example.kotlinfragmentsapp
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -13,18 +13,18 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentTwo.newInstance] factory method to
+ * Use the [FragmentOne.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentTwo : Fragment() {
-    
+class FragmentOne : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+        return inflater.inflate(R.layout.fragment_one, container, false)
     }
 
 }
--- KotlinFragmentsApp/app/src/main/java/com/example/kotlinfragmentsapp/FragmentTwo.kt
+++ KotlinFragmentsApp/app/src/main/java/com/example/kotlinfragmentsapp/FragmentTwo.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinbroadcastapp
+package com.example.kotlinfragmentsapp
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -13,17 +13,19 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentOne.newInstance] factory method to
+ * Use the [FragmentTwo.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentOne : Fragment() {
+class FragmentTwo : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        return inflater.inflate(R.layout.fragment_two, container, false)
     }
+
 
 }
