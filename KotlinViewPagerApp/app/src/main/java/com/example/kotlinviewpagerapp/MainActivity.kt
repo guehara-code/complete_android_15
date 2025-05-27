16d725df45bbd56640ccc0743ae98bcdb70b6137
guehara-code <gil_uehara@yahoo.com.br>
2025-05-27 08:03:22 -0300

    KotlinViewPagerApp - main activity code implemented

--- KotlinViewPagerApp/app/src/main/java/com/example/kotlinviewpagerapp/MainActivity.kt
+++ KotlinViewPagerApp/app/src/main/java/com/example/kotlinviewpagerapp/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewPager2: ViewPager2
+    lateinit var myAdapter: MyPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewPager2 = findViewById(R.id.viewPager2)
+        viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        myAdapter = MyPagerAdapter(supportFragmentManager, lifecycle)
+        myAdapter.addFragmentToList(FragmentOne())
+        myAdapter.addFragmentToList(FragmentTwo())
+        myAdapter.addFragmentToList(FragmentThree())
+
+        viewPager2.adapter = myAdapter
+
     }
 }
