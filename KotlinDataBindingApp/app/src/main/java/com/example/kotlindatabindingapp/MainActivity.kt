5dc7d5e16ba5212971d48f41d7d340be1973d9c6
guehara-code <gil_uehara@yahoo.com.br>
2025-05-31 08:09:48 -0300

    KotlinDataBindingApp - data binding setting and use on main activity

--- KotlinDataBindingApp/app/src/main/java/com/example/kotlindatabindingapp/MainActivity.kt
+++ KotlinDataBindingApp/app/src/main/java/com/example/kotlindatabindingapp/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.kotlindatabindingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+
+        binding.textView1.text = "Hello My Friends"
+
     }
 }
