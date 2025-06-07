c4c4facd3cb138a652e9f9f22c1ba681cbd8dcad
guehara-code <gil_uehara@yahoo.com.br>
2025-06-07 08:05:17 -0300

    KotlinQuadraticEqSolver - solve equation function implement

--- KotlinQuadraticEqSolver/app/src/main/java/com/example/kotlinquadraticeqsolver/MyEquation.kt
+++ KotlinQuadraticEqSolver/app/src/main/java/com/example/kotlinquadraticeqsolver/MyEquation.kt
@@ -1,10 +1,12 @@
 package com.example.kotlinquadraticeqsolver
 
+import android.view.View
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import com.example.kotlinquadraticeqsolver.databinding.ActivityMainBinding
+import kotlin.math.sqrt
 
-class MyEquation: BaseObservable() {
+class MyEquation(var binding: ActivityMainBinding): BaseObservable() {
 
     @Bindable
     var a: String = ""
@@ -26,5 +28,31 @@ class MyEquation: BaseObservable() {
             field = value
             notifyPropertyChanged(BR.c)
         }
-    lateinit var binding: ActivityMainBinding
+
+//    lateinit var binding: ActivityMainBinding
+
+    fun solveEquation(view: View) {
+        var a = a.toInt()
+        var b = b.toInt()
+        var c = c.toInt()
+
+        var discriminant = ((b*b) - (4*a*c))
+
+        var x1: Double
+        var x2: Double
+        if (discriminant > 0) {
+            x1 = (-b + sqrt(discriminant.toDouble()))/(2*a)
+            x2 = (-b - sqrt(discriminant.toDouble()))/(2*a)
+
+            binding.textView.text = "X1 = $x1, X2 = $x2"
+        } else if(discriminant < 0) {
+            binding.resultText.text = "No Real Roots(Complex Roots)"
+        } else {
+            x1 = (-b/(2*a)).toDouble()
+            binding.resultText.text = "x = $x1"
+        }
+
+
+    }
+
 }
