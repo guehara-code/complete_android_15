7934c07a23f159576aef6606ef313b21c8599b7a
guehara-code <gil_uehara@yahoo.com.br>
2025-06-15 08:26:12 -0300

    KotlinContactsManagerApp - DAO class created

---
+++ KotlinContactsManagerApp/app/src/main/java/com/example/kotlincontactsmanagerapp/ContactDAO.kt
@@ -0,0 +1,26 @@
+package com.example.kotlincontactsmanagerapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+interface ContactDAO {
+
+    @Insert
+    suspend fun insertContact(contact: Contact): Long
+
+    @Update
+    suspend fun updateContact(contact: Contact)
+
+    @Delete
+    suspend fun deleteContact(contact: Contact)
+
+    @Query("DELETE FROM contacts_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM contacts_table")
+    fun getAllContactsInDB():LiveData<List<Contact>>
+
+}
