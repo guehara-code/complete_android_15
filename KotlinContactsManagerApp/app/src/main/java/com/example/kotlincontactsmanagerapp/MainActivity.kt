b7a5ae00c8181cc1a14437d87b7e54ad48982e6b
guehara-code <gil_uehara@yahoo.com.br>
2025-06-22 08:18:36 -0300

    KotlinContactsManagerApp - main activity created

--- KotlinContactsManagerApp/app/src/main/java/com/example/kotlincontactsmanagerapp/MainActivity.kt
+++ KotlinContactsManagerApp/app/src/main/java/com/example/kotlincontactsmanagerapp/MainActivity.kt
@@ -5,8 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlincontactsmanagerapp.databinding.ActivityMainBinding
+import com.example.kotlincontactsmanagerapp.repository.ContactRepository
+import com.example.kotlincontactsmanagerapp.room.ContactDatabase
+import com.example.kotlincontactsmanagerapp.viewmodel.ContactViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var contactViewModel: ContactViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var dao = ContactDatabase.getInstance(applicationContext).contactDAO
+        val repository = ContactRepository(dao)
+
+        contactViewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
+
+        binding.contactViewModel = contactViewModel
+
+        binding.lifecycleOwner = this
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.recycleView.layoutManager = LinearLayoutManager(this)
+        DisplayUsersList()
+    }
+
+    private fun DisplayUsersList() {
+        contactViewModel.users.observe(this, {
+            binding.recycleView.adapter = 
+        })
     }
 }
