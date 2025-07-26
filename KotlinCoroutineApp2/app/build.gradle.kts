3b47e52ef6ac0101dde83fb4d32c1859b814df4c
guehara-code <gil_uehara@yahoo.com.br>
2025-07-26 08:24:19 -0300

    KotlinCoroutinesApp - layout created

--- KotlinCoroutineApp2/app/build.gradle.kts
+++ KotlinCoroutineApp2/app/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(libs.kotlin.coroutines.android)
+    implementation(libs.kotlinx.coroutines.android)
 
 }
