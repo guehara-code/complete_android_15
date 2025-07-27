3aa6d810a5c7bf957e37e3acb5ddc738ccc3658c
guehara-code <gil_uehara@yahoo.com.br>
2025-07-27 08:16:14 -0300

    KotlinCoroutinesApp - mainActivity created

--- KotlinCoroutineApp2/app/src/main/java/com/example/kotlincoroutineapp2/MainActivity.kt
+++ KotlinCoroutineApp2/app/src/main/java/com/example/kotlincoroutineapp2/MainActivity.kt
@@ -5,9 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.kotlincoroutineapp2.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +26,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        SayHelloFromMainThread()
+
+        SayHelloFromBackThread()
+
     }
+
+    private fun SayHelloFromMainThread{
+
+        CoroutineScope(Dispatchers.Main).launch {
+            binding.text1.text = "Hello From " +
+                    " ${Thread.currentThread().name}"
+        }
+    }
+
+    private fun SayHelloFromBackThread {
+        CoroutineScope(Dispatchers.IO).launch {
+            binding.text2.text = " Hello From " +
+                    " ${Thread.currentThread().name}"
+        }
+    }
+
+
 }
+
+
+
