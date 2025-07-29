70bc72b67a57554e37a917b1e1096d3d35ea8149
guehara-code <gil_uehara@yahoo.com.br>
2025-07-29 07:51:33 -0300

    KotlinCoroutineSeqApp - sequential functions implementation done

--- KotlinCoroutineSeqApp/app/build.gradle.kts
+++ KotlinCoroutineSeqApp/app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
--- KotlinCoroutineSeqApp/app/src/main/java/com/example/kotlincoroutineseqapp/MainActivity.kt
+++ KotlinCoroutineSeqApp/app/src/main/java/com/example/kotlincoroutineseqapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.kotlincoroutineseqapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val one = doSomethingUsefull1()
+            val two = doSomethingUsefull2()
+
+            val result = one + two
+            Log.v("TAGGY", "The result is : $result")
+        }
+
     }
+
+    suspend fun doSomethingUsefull1(): Int {
+        delay(5000)
+        Log.v("TAGGY", "Fun1 is DONE")
+        return 11
+    }
+
+    suspend fun doSomethingUsefull2(): Int {
+        delay(7000)
+        Log.v("TAGGY", "Fun2 is DONE")
+        return 8
+    }
+
+
 }
