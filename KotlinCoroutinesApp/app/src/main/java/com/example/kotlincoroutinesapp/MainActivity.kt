6d7d88a691f98fa2f0d7a2950587a3b35bf30497
guehara-code <gil_uehara@yahoo.com.br>
2025-08-19 07:28:05 -0300

    KotlinCoroutinesApp - main activity created

--- KotlinCoroutinesApp/app/src/main/java/com/example/kotlincoroutinesapp/MainActivity.kt
+++ KotlinCoroutinesApp/app/src/main/java/com/example/kotlincoroutinesapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kotlincoroutinesapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +20,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var btn = findViewById<Button>(R.id.btn)
+        var textView = findViewById<TextView>(R.id.textView)
+
+        var counter = 0
+
+        btn.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+        }
+
+        downloadBigFileFromNet()
+
     }
+
+    private fun downloadBigFileFromNet() {
+        for (i in 1..1000000) {
+            Log.i("TAGY", "Downloading $i ")
+        }
+    }
+
 }
