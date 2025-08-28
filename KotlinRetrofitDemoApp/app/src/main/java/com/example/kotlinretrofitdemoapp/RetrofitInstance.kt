085e222b4db1681ff7a4dd95e8436b5b0d519412
guehara-code <gil_uehara@yahoo.com.br>
2025-08-28 08:11:25 -0300

    KotlinRetrofitDemoApp - RetrofitInstance class created

---
+++ KotlinRetrofitDemoApp/app/src/main/java/com/example/kotlinretrofitdemoapp/RetrofitInstance.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinretrofitdemoapp
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+
+        val BASE_URL = "https://testapi.devtoolsdaily.com"
+
+        fun getRetrofitInstance(): Retrofit {
+            return  Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(
+                    GsonBuilder().create()
+                )).build()
+        }
+    }
+
+}
