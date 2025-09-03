6aa0478be2fb436b58341365fc2e3dfc33ad2777
guehara-code <gil_uehara@yahoo.com.br>
2025-09-03 08:37:18 -0300

    KotlinRoomApp - DB instance created

---
+++ KotlinRoomApp/app/src/main/java/com/example/kotlinroomapp/MyDB.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinroomapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1)
+abstract class MyDB: RoomDatabase() {
+
+    // Getting DAO instance
+    abstract fun itemDAO(): ItemDao
+    //Accessing the methods to create or get the Database
+    companion object {
+        // Singleton pattern
+        private var Instance: MyDB? = null
+
+        fun getDatabase(context: Context): MyDB {
+            return Instance?: synchronized(this) {
+                androidx.room.Room.databaseBuilder(
+                    context,
+                    MyDB::class.java,
+                    "time_database"
+                ).build()
+                    .also { Instance = it }
+            }
+        }
+
+    }
+
+
+}
