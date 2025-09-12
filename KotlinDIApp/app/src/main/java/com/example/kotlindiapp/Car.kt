b5dffd67a8fb1ed6b12213bc3facb2f811c94d5c
guehara-code <gil_uehara@yahoo.com.br>
2025-09-12 14:35:33 -0300

    KotlinDIApp - DI with method injection implemented

--- KotlinDIApp/app/src/main/java/com/example/kotlindiapp/Car.kt
+++ KotlinDIApp/app/src/main/java/com/example/kotlindiapp/Car.kt
@@ -7,8 +7,13 @@ class Car {
     // Dependency is created inside the class
 //    private val engine = Engine()
 
-    @Inject
+//    @Inject
     lateinit var engine: Engine
+
+    @Inject
+    fun installEngine(engine: Engine) {
+        this.engine = engine
+    }
 
     fun drive() {
         println(engine.start())
