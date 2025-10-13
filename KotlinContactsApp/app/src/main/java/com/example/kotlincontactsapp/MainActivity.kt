1ad9637e3d8cd9ac7bce4410df638d352fc187b2
guehara-code <gil_uehara@yahoo.com.br>
2025-10-13 08:03:42 -0300

    KotlinContactsApp - MainActivity class implemented

--- KotlinContactsApp/app/src/main/java/com/example/kotlincontactsapp/MainActivity.kt
+++ KotlinContactsApp/app/src/main/java/com/example/kotlincontactsapp/MainActivity.kt
@@ -2,19 +2,49 @@ package com.example.kotlincontactsapp
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlincontactsapp.databinding.ActivityMainBinding
+import com.example.kotlincontactsapp.util.ContactAdapter
+import com.example.kotlincontactsapp.viewmodel.ContactViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var adapter: ContactAdapter
+
+    private val viewModel: ContactViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = ContactAdapter {
+            contact -> viewModel.delete(contact)
         }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        viewModel.allContacts.observe(this) {
+            contacts -> adapter.submitList(contacts)
+        }
+
+
+
+
     }
 }
